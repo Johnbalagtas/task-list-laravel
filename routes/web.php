8be465185505a644ac5eaee14ddae4c2298383f6
John <103356254+Johnbalagtas@users.noreply.github.com>
2025-01-06 14:19:40 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,102 @@
+<?php
+
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+
+
+class Task
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $description,
+        public ?string $long_description,
+        public bool $completed,
+        public string $created_at,
+        public string $updated_at
+    ) {}
+}
+
+$tasks = [
+    new Task(
+        1,
+        'Buy groceries',
+        'Task 1 description',
+        'Task 1 long description',
+        false,
+        '2023-03-01 12:00:00',
+        '2023-03-01 12:00:00'
+    ),
+    new Task(
+        2,
+        'Sell old stuff',
+        'Task 2 description',
+        null,
+        false,
+        '2023-03-02 12:00:00',
+        '2023-03-02 12:00:00'
+    ),
+    new Task(
+        3,
+        'Learn programming',
+        'Task 3 description',
+        'Task 3 long description',
+        true,
+        '2023-03-03 12:00:00',
+        '2023-03-03 12:00:00'
+    ),
+    new Task(
+        4,
+        'Take dogs for a walk',
+        'Task 4 description',
+        null,
+        false,
+        '2023-03-04 12:00:00',
+        '2023-03-04 12:00:00'
+    ),
+];
+
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
+Route::get('/tasks', function () use ($tasks) {
+    return view('index', [
+        'tasks' => $tasks
+
+    ]);
+})->name('tasks.index');
+
+Route::get('/task/{id}', function ($id) {
+
+    return view('show', ['task' =>  \App\Models\Task::findOrFail($id)]);
+})->name('tasks.show');
+
+Route::fallback(function () {
+    return "still got somewhere!";
+});
+
+
+Route::get('/test-database', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Database connection is successful';
+    } catch (\Exception $e) {
+        return 'Database connection failed ' . $e->getMessage();
+    }
+});
+
+
+
+// Route::get('/xxx', function () {
+//     return 'Hello';
+// })->name('hello');
+
+// Route::get('hallo', function () {
+//     return redirect()->route('hello');
+// });
+
+// Route::get('/greet/{name}', function ($name) {
+//     return 'Hello ' . $name . '!';
+// });
